30224d2637b3f657c783e07438880e7638692449
Leandro <63678585+Liuth16@users.noreply.github.com>
2025-07-16 16:42:25 -0300

    Add express as alternative to serve the pages

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Informational Node Website
 
 A simple website using Node for Odin Project.
+You can serve the files using node or express, running `node index.js` or `node app.js`
---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "about.html"));
+});
+
+app.get("/contact-me", (req, res) => {
+  res.sendFile(path.join(__dirname, "contact-me.html"));
+});
+
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, "404.html"));
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Running using Express - listening on port ${PORT}`);
+});
